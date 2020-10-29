614371a392a9b1b51b9bd35b715be2b6c3e4b48d
Louise Josephine <loophinesj@gmail.com>
2020-10-29 20:55:17 -0300

    "Diciona cor nos botões e lógica para mudar acor da box ao clicar em uma cor."

--- app/src/main/java/com/example/colormyviews/MainActivity.kt
+++ app/src/main/java/com/example/colormyviews/MainActivity.kt
@@ -5,18 +5,19 @@ import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-    val currentColor = R.color.grey
+    var currentColor = R.color.grey
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         setCollorButtonsAction()
+        setCollorBox()
     }
 
     private fun setCollorButtonsAction() {
         button_red.setOnClickListener {
-           currentColor = R.color.red
+            currentColor = R.color.red
 
         }
 
@@ -29,4 +30,24 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    private fun setCollorBox() {
+        box_one_text.setOnClickListener {
+            box_one_text.setBackgroundResource(currentColor)
+        }
+
+        box_two_text.setOnClickListener {
+            box_two_text.setBackgroundResource(currentColor)
+        }
+
+        box_three_text.setOnClickListener {
+            box_three_text.setBackgroundResource(currentColor)
+        }
+
+        box_four_text.setOnClickListener {
+            box_four_text.setBackgroundResource(currentColor)
+        }
+        box_five_tex.setOnClickListener {
+            box_five_tex.setBackgroundResource(currentColor)
+        }
+    }
 }
