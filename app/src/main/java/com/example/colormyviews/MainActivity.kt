7fd03790c19bf2441b1d1122ca3fac6e6f3de6ae
Louise Josephine <loophinesj@gmail.com>
2020-10-29 20:07:35 -0300

    "Diciona botões e lógica para mudar a cor deles"

--- app/src/main/java/com/example/colormyviews/MainActivity.kt
+++ app/src/main/java/com/example/colormyviews/MainActivity.kt
@@ -2,10 +2,31 @@ package com.example.colormyviews
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    val currentColor = R.color.grey
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setCollorButtonsAction()
     }
+
+    private fun setCollorButtonsAction() {
+        button_red.setOnClickListener {
+           currentColor = R.color.red
+
+        }
+
+        button_yellow.setOnClickListener {
+            currentColor = R.color.yellow
+        }
+
+        button_green.setOnClickListener {
+            currentColor = R.color.green
+        }
+    }
+
 }
